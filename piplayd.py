e54627aa93a41e8f255974d982f0b30b18bb31bf
Francesco Ruvolo <ruvolof@gmail.com>
2013-10-04 10:28:16 +0200

    more standard script structure

--- piplayd.py
+++ piplayd.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 
 from Tkinter import *
+import sys, getopt 
 import tkSnack as sndsys
-import sys
-from time import sleep
 
-# Creating Tk window for Snack
-win = Tk()
+def main():
+    # Creating Tk window for Snack
+    win = Tk()
 
-# Initializing Snack
-sndsys.initializeSnack(win)
+    # Initializing Snack
+    sndsys.initializeSnack(win)
 
-# Withdrawing Tk window, we don't need it after initializeSnack()
-win.withdraw()
+    # Withdrawing Tk window, we don't need it after initializeSnack()
+    win.withdraw()
 
-# Initiliazing Sound object
-mp3 = sndsys.Sound()
+    # Initiliazing Sound object
+    mp3 = sndsys.Sound()
 
-def playSong(path):
-    mp3.flush()
-    mp3.read(path)
-    mp3.play()
-    print "Now playing: %s" % (path)
+    def playSong(path):
+        mp3.flush()
+        mp3.read(path)
+        mp3.play()
+        print "Now playing: %s" % (path)
 
-playSong(sys.argv[1])
+    playSong(sys.argv[1])
 
-mainloop()
+    mainloop()
+
+if __name__ == "__main__":
+    main()
