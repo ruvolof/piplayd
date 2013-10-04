25de7084f6cd494ac72cc29804c354f9985ceac1
Francesco Ruvolo <ruvolof@gmail.com>
2013-10-04 10:45:18 +0200

    getopt for port and docroot added

--- piplayd.py
+++ piplayd.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 
 from Tkinter import *
-import sys, getopt 
+import sys, getopt , os
 import tkSnack as sndsys
 
 def main():
+    # Catching command line options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:r:")
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
+
+    # default values
+    port = 9580
+    docroot = os.getcwd()
+
+    # Parsing command line options
+    for o, a in opts:
+        if o == "-p":
+            port = int(a)
+        elif o == "-r":
+            docroot = a
+        else:
+            assert False, "Unhandled option"
+
+    print "%d %s" % (port, docroot)
+
     # Creating Tk window for Snack
     win = Tk()
 
@@ -23,7 +45,7 @@ def main():
         mp3.play()
         print "Now playing: %s" % (path)
 
-    playSong(sys.argv[1])
+    playSong(args[0])
 
     mainloop()
 
